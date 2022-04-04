9da58b5c202a46dcf58da3a9e9a8c4f0aa750577
matumoto1234 <matumotosizuoka@gmail.com>
2022-04-05 04:01:08 +0900

    Create main.ts

---
+++ temp-entrance2022/src/main.ts
@@ -0,0 +1,66 @@
+const properties = PropertiesService.getScriptProperties().getProperties();
+const WEBHOOK_URL: string = properties.WEBHOOK_URL;
+
+type FormResponse = string | string[] | string[][];
+
+const formResponseToString = (s: FormResponse): string => {
+  switch (typeof s) {
+    case 'string':
+      return s;
+    case 'object':
+      switch (typeof s[0]) {
+        case 'string':
+          return s.join('\n');
+        case 'object':
+          // 二次元配列を一次元にしてそれを'\n'区切りのstringに
+          return [].concat(s).join('\n');
+        default:
+          return '';
+      }
+    default:
+      return '';
+  }
+};
+
+const onSubmit = (e: any) => {
+  const response: GoogleAppsScript.Forms.FormResponse = e.response;
+  const itemResponses: GoogleAppsScript.Forms.ItemResponse[] = response.getItemResponses();
+
+  let respondentName: string = '';
+  let studentNumber: string = '';
+
+  for (const itemResponse of itemResponses) {
+    const title: string = itemResponse.getItem().getTitle();
+    const res: FormResponse = itemResponse.getResponse();
+    const strRes: string = formResponseToString(res);
+
+    if (title === '名前(本名)') {
+      respondentName = strRes;
+    }
+
+    if (title === '学籍番号') {
+      studentNumber = strRes;
+    }
+  }
+
+  const heading: string = '仮入部フォームに回答がありました。\n';
+  const name: string = '名前 : ' + respondentName + '\n';
+  const stuNum: string = '学籍番号 : ' + studentNumber + '\n';
+
+  const data: string = heading + name + stuNum;
+
+
+  const body: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
+    method: 'post',
+    contentType: 'application/json',
+    payload: JSON.stringify({
+      content: data
+    })
+  }
+
+  UrlFetchApp.fetch(WEBHOOK_URL, body)
+}
+
+const main = (e: any) => {
+  onSubmit(e);
+};
